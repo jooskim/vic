155da12728a6b695f977708898be2d4e8ea168f1
Josh Kim <kjosh@vmware.com>
2016-07-14 14:01:25 -0500

    Add copyright statement for shell and batch files

--- ui/vic-ui/build-flex.bat
+++ ui/vic-ui/build-flex.bat
@@ -1,6 +1,17 @@
 @echo off
-REM --- Windows script starting an Ant build of the current flex project
-REM --- (if Ant runs out of memory try defining ANT_OPTS=-Xmx512M)
+REM Copyright 2016 VMware, Inc. All Rights Reserved.
+REM
+REM Licensed under the Apache License, Version 2.0 (the "License");
+REM you may not use this file except in compliance with the License.
+REM You may obtain a copy of the License at
+REM
+REM    http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing, software
+REM distributed under the License is distributed on an "AS IS" BASIS,
+REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM See the License for the specific language governing permissions and
+REM limitations under the License.
 
 @IF not defined ANT_HOME (
    @echo BUILD FAILED: You must set the env variable ANT_HOME to your Apache Ant folder
