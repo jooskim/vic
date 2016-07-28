e79d3aba77307e610a5fa8c591c0eb51fa3d893c
Josh Kim <kjosh@vmware.com>
2016-07-28 15:47:08 -0500

    Added a NGC automation project for VIC UI

---
+++ ui/vic-uia/src/main/java/com/vmware/vsphere/client/automation/vicui/vchportletexists/VchPortletExistsTest.java
@@ -0,0 +1,100 @@
+package com.vmware.vsphere.client.automation.vicui.vchportletexists;
+
+import org.testng.annotations.Test;
+import com.vmware.client.automation.common.spec.TaskSpec;
+import com.vmware.client.automation.common.step.VerifyEntityNameByUiStep;
+import com.vmware.client.automation.common.step.VerifyTaskByUiStep;
+import com.vmware.client.automation.workflow.BaseTest.TestID;
+import com.vmware.client.automation.workflow.common.WorkflowSpec;
+import com.vmware.client.automation.workflow.common.WorkflowStepsSequence;
+import com.vmware.client.automation.workflow.explorer.TestBedBridge;
+import com.vmware.client.automation.workflow.explorer.TestbedSpecConsumer;
+import com.vmware.client.automation.workflow.test.TestWorkflowStepContext;
+import com.vmware.vsphere.client.automation.common.step.ClickNextWizardButtonStep;
+import com.vmware.vsphere.client.automation.common.workflow.NGCTestWorkflow;
+import com.vmware.vsphere.client.automation.components.navigator.NGCNavigator;
+import com.vmware.vsphere.client.automation.components.navigator.spec.ClusterLocationSpec;
+import com.vmware.vsphere.client.automation.components.navigator.spec.VmLocationSpec;
+import com.vmware.vsphere.client.automation.components.navigator.step.ClusterNavigationStep;
+import com.vmware.vsphere.client.automation.components.navigator.step.HostNavigationStep;
+import com.vmware.vsphere.client.automation.components.navigator.step.VmNavigationStep;
+import com.vmware.vsphere.client.automation.provider.commontb.CommonTestBedProvider;
+import com.vmware.vsphere.client.automation.srv.common.spec.ClusterSpec;
+import com.vmware.vsphere.client.automation.srv.common.spec.DatacenterSpec;
+import com.vmware.vsphere.client.automation.srv.common.spec.DatastoreSpec;
+import com.vmware.vsphere.client.automation.srv.common.spec.SpecFactory;
+import com.vmware.vsphere.client.automation.srv.common.spec.VcSpec;
+import com.vmware.vsphere.client.automation.srv.common.step.VerifyVmExistenceByApiStep;
+import com.vmware.vsphere.client.automation.vm.common.VmUtil;
+import com.vmware.vsphere.client.automation.vm.lib.createvm.spec.CreateVmSpec;
+import com.vmware.vsphere.client.automation.vm.lib.createvm.spec.CreateVmSpec.VmCreationType;
+
+/**
+ * import vicui-specific spec, steps here
+*/
+
+/**
+ * Test class for create VM in the NGC client.
+ * Executes the following test work-flow:
+ *  1. Open a browser
+ *  2. Login as admin user
+ *  3. Navigate to the cluster
+ *  4. -
+ */ 
+
+public class VchPortletExistsTest extends NGCTestWorkflow {
+	protected static final String TAG_SUMMARY_TAB = "TAG_SUMMARY_TAB";
+	
+	@Override
+	public void initSpec(WorkflowSpec testSpec, TestBedBridge testbedBridge) {
+		TestbedSpecConsumer testBed = testbedBridge.requestTestbed(CommonTestBedProvider.class, true);
+		
+		// Spec for the VC
+	    VcSpec requestedVcSpec = testBed.getPublishedEntitySpec(CommonTestBedProvider.VC_ENTITY);
+
+	    // Spec for the datacenter
+	    DatacenterSpec requestedDatacenterSpec = testBed.getPublishedEntitySpec(CommonTestBedProvider.DC_ENTITY);
+
+	    // Spec for the cluster
+	    ClusterSpec requestedClusterSpec = testBed.getPublishedEntitySpec(CommonTestBedProvider.CLUSTER_ENTITY);
+	    
+	    // Spec for the datastore
+	    DatastoreSpec requestedDatastoreSpec = testBed.getPublishedEntitySpec(CommonTestBedProvider.CLUSTER_HOST_DS_ENTITY);
+	    
+	    // Spec for the VM that will be created
+	    CreateVmSpec createVmSpec = SpecFactory.getSpec(CreateVmSpec.class, requestedClusterSpec);
+	    createVmSpec.creationType.set(VmCreationType.CREATE_NEW_VM);
+	    createVmSpec.datastore.set(requestedDatastoreSpec);
+	      
+	    // Spec for the location to the Cluster
+	    ClusterLocationSpec clusterLocationSpec = new ClusterLocationSpec(requestedClusterSpec);
+	    ClusterLocationSpec clusterSummaryLocationSpec = new ClusterLocationSpec(requestedClusterSpec, NGCNavigator.NID_ENTITY_PRIMARY_TAB_SUMMARY);
+	    clusterSummaryLocationSpec.tag.set(TAG_SUMMARY_TAB);
+
+	    // Spec for the location to the VM
+	    VmLocationSpec vmLocationSpec = new VmLocationSpec(createVmSpec);
+
+	    // Spec for the create VM task
+	    TaskSpec createVmTaskSpec = new TaskSpec();
+//	    createVmTaskSpec.name.set(VmUtil.getLocalizedString("task.createVm.name"));
+//	    createVmTaskSpec.status.set(TaskSpec.TaskStatus.COMPLETED);
+//	    createVmTaskSpec.target.set(requestedDatacenterSpec);
+	    testSpec.add(requestedVcSpec, requestedDatacenterSpec, requestedClusterSpec, createVmSpec, clusterLocationSpec, vmLocationSpec, createVmTaskSpec);
+	    super.initSpec(testSpec, testbedBridge);
+	}
+	
+	@Override
+	public void composeTestSteps(WorkflowStepsSequence<TestWorkflowStepContext> flow) {
+		super.composeTestSteps(flow);
+		
+//		flow.appendStep("Navigating to Cluster view", new ClusterNavigationStep());
+		flow.appendStep("Navigating to Cluster view summary tab", new ClusterNavigationStep(), new String[] { TAG_SUMMARY_TAB }); 
+	}
+	
+	@Override
+	@Test(description = "Check if VCH portlet is visible")
+	@TestID(id = "0")
+	public void execute() throws Exception {
+		super.execute();
+	}
+}
