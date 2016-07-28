b1a79432207df0af4626fafe1bebb444996370d6
Josh Kim <kjosh@vmware.com>
2016-07-28 15:47:08 -0500

    Merged NGC automation branch
    [wip] Added drone snippet for uia build
    
    Dropped unnecessary repositories tag in vic-uia/pom.xml
    
    Added TestNG config file
    
    [WIP] Wrote some initial test cases for UIA
    
    Trying to get around the framework bug to get the 'Summary' tab to work
    
    Writing basic tests for VIC UI
    
    Currently testing under the version that only supports
    vCenter 6.5. Need to talk more with the Sofia team for 6.0 support
    
    [WIP] Supporting testing in VC 6.0
    
    Made the test work with mvn test
    
    Made testng.xml configurable
    
    This will allow the Drone CI to provide VM names at runtime
    
    [WIP] Use vicEnvProvider.properties
    
    Need to implement VM creation logic
    
    Made TestNG fail on config failure

---
+++ ui/vic-uia/src/main/java/com/vmware/vsphere/client/automation/vicui/common/VicUIConstants.java
@@ -0,0 +1,7 @@
+package com.vmware.vsphere.client.automation.vicui.common;
+
+import com.vmware.suitaf.apl.IDGroup;
+
+public class VicUIConstants {
+	public static final String VC_VERSION_6_0 = "6.0";
+}
---
+++ ui/vic-uia/src/main/java/com/vmware/vsphere/client/automation/vicui/common/VicUITestWorkflow.java
@@ -0,0 +1,37 @@
+package com.vmware.vsphere.client.automation.vicui.common;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+
+import com.vmware.client.automation.workflow.common.WorkflowStepsSequence;
+import com.vmware.client.automation.workflow.explorer.TestbedSpecConsumer;
+import com.vmware.client.automation.workflow.test.TestWorkflowStepContext;
+import com.vmware.vsphere.client.automation.common.workflow.NGCTestWorkflow;
+import com.vmware.vsphere.client.automation.srv.common.spec.UserSpec;
+import com.vmware.vsphere.client.automation.srv.common.spec.VcSpec;
+
+public class VicUITestWorkflow extends NGCTestWorkflow {
+	/*
+	 * This disables admin user creation and forces the framework to use the administrator@vsphere.local account because 6.0 has an issue with SSO 
+	 */
+	
+	protected static String _containerVmName;
+	protected static String _vchVmName;
+	protected static boolean _isVcVersion6_0;
+	
+	@Override
+	protected UserSpec generateUserSpec(VcSpec vcSpec) {
+		UserSpec userSpec = new UserSpec();
+		userSpec.username.set("administrator@vsphere.local");
+		userSpec.password.set("Admin!23");
+		userSpec.parent.set(vcSpec);
+		userSpec.tag.set(NGCTestWorkflow.TEST_USER_SPEC_TAG);
+		
+		return userSpec;
+	}
+	
+	@Override
+	public void composePrereqSteps(WorkflowStepsSequence<TestWorkflowStepContext> flow) {
+		
+	}
+}
