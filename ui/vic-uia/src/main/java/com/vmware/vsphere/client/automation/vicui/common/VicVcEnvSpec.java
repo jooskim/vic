1c582bd37f3e8292a1627935c5ecfabaef5371c1
Josh Kim <kjosh@vmware.com>
2016-07-28 15:47:08 -0500

    Merged NGC automation branch
    [wip] Added drone snippet for uia build
    
    Dropped unnecessary repositories tag in vic-uia/pom.xml
    
    Added TestNG config file
    
    [WIP] Wrote some initial test cases for UIA
    
    Trying to get around the framework bug to get the 'Summary' tab to work
    
    Writing basic tests for VIC UI
    
    Currently testing under the version that only supports
    vCenter 6.5. Need to talk more with the Sofia team for 6.0 support
    
    [WIP] Supporting testing in VC 6.0
    
    Made the test work with mvn test
    
    Made testng.xml configurable
    
    This will allow the Drone CI to provide VM names at runtime
    
    [WIP] Use vicEnvProvider.properties
    
    Need to implement VM creation logic
    
    Made TestNG fail on config failure

---
+++ ui/vic-uia/src/main/java/com/vmware/vsphere/client/automation/vicui/common/VicVcEnvSpec.java
@@ -0,0 +1,11 @@
+package com.vmware.vsphere.client.automation.vicui.common;
+
+import com.vmware.client.automation.common.spec.EntitySpec;
+import com.vmware.hsua.common.datamodel.BasePBox.DataProperty;
+import com.vmware.vsphere.client.automation.srv.common.spec.VmSpec;
+
+public class VicVcEnvSpec extends EntitySpec {
+	public DataProperty<String> vcVersion;
+	public DataProperty<String> vchVmName;
+	public DataProperty<String> containerVmName;
+}
